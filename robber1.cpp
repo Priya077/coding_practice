ffa1d0ae4fe74ee3198dde2d2b6b9618cb421664
Priya077 <78656082+Priya077@users.noreply.github.com>
2022-06-22 22:38:10 +0530

    Add files via upload

---
+++ robber1.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int rob(vector<int>& nums) {
+    int n=nums.size();
+    if(n==1) return nums[0];
+    //else if(n==2) return max(nums[0],nums[1]);
+    int dp[n];
+    dp[0]=nums[0],dp[1]=max(nums[0],nums[1]);
+    for(int i=2;i<n;i++)
+    {
+        dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+    }
+    return dp[n-1];
+}
+
+int main()
+{
+
+    int N,x;
+    cin>>N;
+    vector<int>nums;
+    for(int i=0;i<N;i++)
+    {
+
+        cin>>x;
+        nums.push_back(x);
+    }
+    cout<<rob(nums);
+    return 0;
+}
